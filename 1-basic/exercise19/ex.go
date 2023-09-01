3b444c17494e110e3487768a2642b185beb2e207
lpiasseta <leandro.piasseta@mercadolivre.com>
2023-09-01 10:08:00 -0300

    add ex19

--- 1-basic/exercise19/ex.go
+++ 1-basic/exercise19/ex.go
@@ -5,34 +5,27 @@ import (
 )
 
 func RunExercise19() {
-	// var start, end int
+	var start, end int
 
-	// fmt.Println("Please choose the range of numbers for which you'd like to generate the multiplication table.")
-	// fmt.Print("Enter the starting number: ")
-	// fmt.Scanln(&start)
+	fmt.Println("Please choose the range of numbers for which you'd like to generate the multiplication table.")
+	fmt.Print("Enter the starting number: ")
+	fmt.Scanln(&start)
 
-	// fmt.Print("Enter the ending number: ")
-	// fmt.Scanln(&end)
+	fmt.Print("Enter the ending number: ")
+	fmt.Scanln(&end)
 
-	var header string
-
-	start := 1
-	end := 10
-
-	for i := start; i < end+1; i++ {
-		header += fmt.Sprintf("  |%v", i)
+	fmt.Printf("  |")
+	for i := start; i <= end; i++ {
+		fmt.Printf("%4d", i)
 	}
+	fmt.Println("\n-------------------------------------------")
 
-	fmt.Println(header)
-	fmt.Println("------------------------------------------")
-	var line string
-
-	for i := start; i < end+1; i++ {
-		line += fmt.Sprintf("\n%v|", i)
-		for j := start; j < end+1; j++ {
-			line += fmt.Sprintf("  %v", j*i)
+	for i := start; i <= end; i++ {
+		fmt.Printf("%2d|", i)
+		for j := start; j <= end; j++ {
+			result := i * j
+			fmt.Printf("%4d", result)
 		}
+		fmt.Println()
 	}
-
-	fmt.Println(line)
 }
---
+++ 1-basic/exercise20/ex.go
@@ -0,0 +1,11 @@
+package exercise20
+
+import "fmt"
+
+func RunExercise20() {
+	var str string
+
+	fmt.Println("")
+	fmt.Scanln(&)
+
+}
